77a18d2f44697ce3bc80d11303469c299d5f5c15
Nuku Ameyibor <nayibor@gmail.com>
2016-09-14 02:33:16 +0000

    temp file resurrected

---
+++ yapp_test/priv/docroot/webroot/js/temp.js
@@ -0,0 +1,192 @@
+
+var temp = {
+	
+edit_temp_div:("#edit_temp_div"),
+
+configure_temp_div:function(){
+		
+					  var diag = $(temp.edit_temp_div);
+			        
+			          diag.dialog({
+						width: 500,
+						height: 400,
+						position:"center",
+						modal:false,
+			            buttons: {
+							Cancel:function(){
+							    $( this ).dialog( "close" );	
+							},
+							Save: function() {
+								temp.checkfields();
+			                }
+			               
+			            }
+			        });
+			    
+			        diag.dialog('close');	
+        
+		},
+		
+checkfields:function(){
+	
+					_this=this;	
+					var link=$("#save_add_temp").val(); 
+					var counter=0;
+					$(".ca").each(function(){
+						if(!(document.getElementById($(this).attr("id")).checkValidity())){
+							$(this).css("border","solid #F44 2px"); 
+							counter++;
+						}else
+						{
+							$(this).css("border","solid grey 1px");       
+						}
+					});
+					  
+					if(counter==0)
+					{
+						temp.saveTemp(link);
+					}
+					else{
+			
+						settings.show_message("Please Enter All Fields In Correct Format");
+					}
+	
+		},		
+		
+		
+saveTemp:function(link){
+		
+					var formdata=$("#add_temp_form.cmxform").serialize(); 
+						$.ajax({
+				            url: link,
+				            type:"POST",
+				            dataType:"html",
+				            data:formdata,
+				            //data:(val!="") ? "filter="+val : "",
+				            beforeSend:function(){
+				               settings.disable_okbutt_mgdialg() ;
+				               settings.show_message("Saving Data");    
+				            },
+				            success:function(Data) {
+				             
+				                settings.show_message(Data); 
+				                setTimeout(function() {
+								$(temp.edit_temp_div).dialog('close');	                   
+								temp.load_temp($("#search_temp_url").val()); 
+							}, 2000);	
+				               //$("#add_edit_user").html(Data);
+				               //$(_this.add_edit_diag).dialog('close');
+				       
+				            },
+				            error:function(Data){
+				               settings.enable_okbutt_mgdialg();
+				               settings.show_message(Data.responseText);      
+				            }
+				        }); 
+		
+		}		
+		
+		,
+
+init:function(){
+	
+	
+					 temp.configure_temp_div();
+		
+				 //searching for a link when the search button is clicked
+			          $("#search_temp_butt").live('click',function(e) {
+			            e.preventDefault();
+			            var link = $("#search_temp_url").val();
+			              temp.load_temp(link); 
+					}); 
+			        
+			       
+			        
+			           //for searching for a link when the enter button is presed
+			        $("#add_temp").live('click',function(e) {
+			            e.preventDefault();
+			            var link = $("#get_add_temp").val();
+			            temp.load_addtemp(link); 
+			        }); 
+			        
+			        
+			         //for searching for a link when the enter button is presed
+			        $("#search_template").live('keyup',function(e) {
+			            e.preventDefault();
+			            var link = $("#search_temp_url").val();
+			            if(e.which==13){
+			              temp.load_temp(link); 
+			            }
+			        });	
+			        
+				},
+				
+				
+load_addtemp:function(link){
+		
+					$.ajax({
+			            url: link,
+			            type:"GET",
+			            dataType:'html',
+			            beforeSend:function(){
+			              // _this.disable_okbutt_mgdialg() ;
+			              // _this.show_message("Retrieving Add Modal...");    
+			            },
+			            success:function(Data) {
+			               settings.close_message_diag();
+			               $("#edit_temp_div").html(Data);
+			               $(temp.edit_temp_div).dialog('open');
+			       
+			            },
+			            error:function(Data){
+			               settings.enable_okbutt_mgdialg();
+			               settings.show_message("Error<br>"+"Please Try Again");      
+			            }
+			        }); 		
+					
+				},
+					
+        
+load_temp:function(link){
+		
+					 val = $("#search_template").val();
+					 $.ajax({
+			            url: link,
+			            type:"GET",
+			            dataType:'html',
+			            data:(val!="") ? "filter="+val : "",
+			            beforeSend:function(){
+			               settings.disable_okbutt_mgdialg() ;
+			               settings.show_message("Searching Templates...");    
+			            },
+			            success:function(Data) {
+			                settings.close_message_diag();
+			               settings.enable_okbutt_mgdialg();
+			               $("#table_info").html(Data);
+			       
+			            },
+			            error:function(Data){
+			               settings.enable_okbutt_mgdialg();
+			               settings.show_message("Error<br>"+"Please Try Again");      
+			            }
+			        }); 	
+		        	
+		}
+        
+        
+		
+		}
+	
+	
+	
+	
+	
+
+$(document).ready(function(){
+    
+    temp.init();  
+
+});
+
+	
+
