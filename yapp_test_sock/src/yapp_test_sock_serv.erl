970f0fe2042490b9a547b99cd7d03930b553c44a
Nuku Ameyibor <nayibor@gmail.com>
2016-09-27 04:55:02 +0000

    %%change%%
    	stub of how specification will be implemented
    	excel file with interesting sample

--- yapp_test_sock/src/yapp_test_sock_serv.erl
+++ yapp_test_sock/src/yapp_test_sock_serv.erl
@@ -58,12 +58,58 @@ handle_info(?SOCK(Str), S = #state{socket=AcceptSocket,iso_message=Isom}) ->
 				Len = lists:nth(2,LenStr) + 2,
 				case length(State_new) of 
 					SizeafterHead when Len =:= SizeafterHead ->
-					io:format("~nlength is ~p ~nfull message is:~n~s", [Len-2,Rest]),
-					io:format("~nrequest_mti:~s",[lists:sublist(Rest,4)]),
-					io:format("~nin progress primary bitmap is ~p ",[lists:sublist(Rest,5,16)]),
-					io:format("~nprimary is ~p ",[lists:map(fun(X)->string:right(integer_to_list(X,2),8,$0)end,lists:sublist(Rest,5,8))]),
-					io:format("~nflattend primary bitmap in binary  is ~p ",[lists:flatten(lists:map(fun(X)->string:right(integer_to_list(X,2),8,$0)end,lists:sublist(Rest,5,8)))]),
-					io:format("~nfirst value is ~p and first binary bitmap:~p and hex value is ~p",[lists:nth(5,Rest),string:right(integer_to_list(lists:nth(5,Rest),2),8,$0),integer_to_list(lists:nth(5,Rest),16)]),
+					Mti_size = 4,
+					io:format("~n~nlength is ~p ~nfull message is:~n~s", [Len-2,Rest]),
+					io:format("~n~nrequest_mti:~p",[lists:sublist(Rest,Mti_size)]),
+					io:format("~n~nfirst twelve integer values are:~p",[lists:sublist(Rest,1,12)]),
+					io:format("~n~nin progress primary bitmap are:~p",[lists:sublist(Rest,Mti_size+1,8)]),
+					%%gets whether the  first character of the bitmap is 1/0(48,49)cuz of string and then uses this to calculate size of bitmap 
+					Bitmap_size = case lists:nth(1,string:right(integer_to_list(lists:nth(5,Rest),2),8,$0)) of
+									48 -> 8;
+									49 -> 16
+									end,
+					io:format("~n~nbitmap size is:~p",[Bitmap_size]),
+					io:format("~n~nbitmap(pri/sec) is :~p",[lists:map(fun(X)->string:right(integer_to_list(X,2),8,$0)end,lists:sublist(Rest,Mti_size+1,Bitmap_size))]),
+					io:format("~n~nflattend bitmap(pri/sec) in binary is:~p",[lists:flatten(lists:map(fun(X)->string:right(integer_to_list(X,2),8,$0)end,lists:sublist(Rest,Mti_size+1,Bitmap_size)))]),
+					%%io:format("~nfirst value is ~p and first binary bitmap:~p and hex value is ~p",[lists:nth(5,Rest),string:right(integer_to_list(lists:nth(5,Rest),2),8,$0),integer_to_list(lists:nth(5,Rest),16)]),
+					
+					%%calculate the data elements and their value 
+					%%will have to actually use a formual to get the values programmatically out of message
+					%%wont be too difficult but have to be careful about how i create the data elements 
+					
+					%%to obtain value for particular data element you need(stub of how specification will be implemented )
+							%%check whether that data element exists
+							%%if exists get postion to start from 
+							%%atrributes of that data element to see how you will obtain data for it and calcullate value for next person in sequence 
+							%%foldr will be used but have to be careful about nature of fold
+							%%header file will have to be created with definitions for each fld and the attributes of that field value according to  a specification 
+							%%three main features of a data item are total length,fixed or variable,padding type,data type(numeric,binary,etc..)
+							%%lists:foldl(fun(X, Sum) -> X + Sum end, 0, [1,2,3,4,5]).
+							
+							%%listsfoldl(fun(X,Acc={Data_for_use_in,Index_start_in,Current_index_in,Map_out_list_in})->
+							%%						
+							%%						Fld_attr=Get_att_value(Current_index_in),
+							%%						NextVal_start_pos=Fld_atttr.get_nextval(),
+							%%						Fld_length=Fld_atttr.get_length()+padding_if_Avail,
+							%%						get value here,
+							%%						Fld_num_out=Current_index+1,
+							%%						{Data_for_use_out,Index_start_out,Fld_num_out,Map_out_list_out}end,
+							%%						{Data_for_use,Index_start,Fld_num,Map_out_list},
+							%%						bitmap__base10_list);
+							
+							
+					io:format("~n~n~n###below are the data elements for above bitmap###"),
+					io:format("~npan digits ~p and pan is:~p",[lists:sublist(Rest,Mti_size+Bitmap_size+1,2),lists:sublist(Rest,Mti_size+Bitmap_size+3,16)]),
+					io:format("~nprocessing code is:~p",[lists:sublist(Rest,31,6)]),
+					io:format("~namount for trasaction is:~p",[lists:sublist(Rest,37,12)]),
+					io:format("~nstan for trasaction is:~p",[lists:sublist(Rest,49,6)]),
+					io:format("~ntimestamp local trasaction is:~p",[lists:sublist(Rest,55,12)]),
+					io:format("~npan expiry is:~p",[lists:sublist(Rest,67,4)]),
+					io:format("~npos data code is:~p",[lists:sublist(Rest,71,12)]),
+
+
+
+					
 						% Display the MTI from the response.
 						{noreply, S#state{iso_message=[]}};
 					SizeafterHead when Len < SizeafterHead ->
