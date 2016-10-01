05fde47aff8318fac3125e5621b222d2f021bdde
Nuku Ameyibor <nayibor@gmail.com>
2016-10-01 05:36:49 +0000

    %changes 	iso message processing moved to a differnt file(ascii_marsh) 	exception handling added to iso processing

---
+++ yapp_test_sock/src/yapp_test_ascii_marsh.erl
@@ -0,0 +1,78 @@
+%%%
+%%% @doc yapp_test_sock_serv module.
+%%%<br>this module is responsible for processing iso messages</br>
+%%% @end
+%%% @copyright Nuku Ameyibor <nayibor@startmail.com>
+
+
+-module(yapp_test_ascii_marsh).
+
+
+
+-export([process_iso_message/1]).
+-include_lib("yapp_test_sock/include/yapp_test_sock_spec.hrl").
+
+-define(MTI_SIZE,4).
+-define(PRIMARY_BITMAP_SIZE,8).
+
+
+
+-spec process_iso_message([pos_integer()])->map().
+process_iso_message(Rest)->
+		
+		Mti_size = ?MTI_SIZE,
+		Primary_Bitmap_size = ?PRIMARY_BITMAP_SIZE,
+		%io:format("~n~nlength is ~p ~nfull message is:~n~s", [Len-2,Rest]),
+		%io:format("~n~nrequest_mti:~p",[lists:sublist(Rest,Mti_size)]),
+		%io:format("~n~nfirst twelve integer values are:~p",[lists:sublist(Rest,1,12)]),
+		%io:format("~n~nin progress primary bitmap are:~p",[lists:sublist(Rest,Mti_size+1,8)]),
+		%%gets whether the  first character of the bitmap is 1/0(48,49)cuz of string and then uses this to calculate size of bitmap 
+		Bitmap_size = case lists:nth(1,string:right(integer_to_list(lists:nth(5,Rest),2),8,$0)) of
+						48 -> 8;
+						49 -> 16
+						end,
+		%io:format("~n~nbitmap size is:~p",[Bitmap_size]),
+		%io:format("~n~nbitmap(pri/sec) is :~p",[lists:map(fun(X)->string:right(integer_to_list(X,2),8,$0)end,lists:sublist(Rest,Mti_size+1,Bitmap_size))]),
+		%io:format("~n~nflattend bitmap(pri/sec) in binary is:~p",[lists:flatten(lists:map(fun(X)->string:right(integer_to_list(X,2),8,$0)end,lists:sublist(Rest,Mti_size+1,Bitmap_size)))]),
+		%%io:format("~nfirst value is ~p and first binary bitmap:~p and hex value is ~p",[lists:nth(5,Rest),string:right(integer_to_list(lists:nth(5,Rest),2),8,$0),integer_to_list(lists:nth(5,Rest),16)]),
+		Bitmap_transaction = lists:flatten(lists:map(fun(X)->string:right(integer_to_list(X,2),8,$0)end,lists:sublist(Rest,Mti_size+1,Bitmap_size))),
+		%io:format("~nraw values ~w int value to use  ~p asci value code is:~p",[lists:sublist(Rest,68,8),list_to_integer(lists:sublist(Rest,68,2)),lists:sublist(Rest,68,8)]),
+		
+		%%add bitmap as well as mti to map which holds data elements so they can help in processing rules 
+		Mti_Data_Element = maps:from_list([{ftype,ans},{fld_no,0},{name,<<"Mti">>},{val_binary_form,list_to_binary(lists:sublist(Rest,Mti_size))},{val_list_form,lists:sublist(Rest,Mti_size)}]),
+		Bitmap_Data_ELement = maps:from_list([{ftype,b},{fld_no,0},{name,<<"Bitmap">>},{val_binary_form,list_to_binary(Bitmap_transaction)},{val_list_form,Bitmap_transaction}]),
+		Map_Data_Element1 =  maps:put(<<"_mti">>,Mti_Data_Element,maps:new()), 
+		Map_Data_Element = maps:put(<<"_bitmap">>,Bitmap_Data_ELement,Map_Data_Element1),
+		
+		
+		Start_index = Mti_size+Primary_Bitmap_size+1,
+		%%Bitmap_transaction_test = lists:sublist(Bitmap_transaction,5),
+		OutData =lists:foldl(fun(X,_Acc={Data_for_use_in,Index_start_in,Current_index_in,Map_out_list_in}) when X =:= 49->						
+								    {Ftype,Flength,Fx_var_fixed,Fx_header_length,DataElemName}=?SPEC(Current_index_in),
+									case Fx_var_fixed of
+										fx -> 
+											Data_Element = lists:sublist(Rest,Index_start_in,Flength),
+											New_Index = Index_start_in+Flength ;	
+										vl ->
+											Vl_value = list_to_integer(lists:sublist(Rest,Index_start_in,Fx_header_length)),
+											Start_val = Index_start_in + Fx_header_length , 										
+											Data_Element = lists:sublist(Rest,Start_val,Vl_value),
+											New_Index = Start_val+Vl_value
+									end,
+									NewData  = maps:from_list([{ftype,Ftype},{fld_no,Current_index_in},{name,DataElemName},{val_list_form,Data_Element}]),
+									NewMap = maps:put("_"++integer_to_list(Current_index_in),NewData,Map_out_list_in),
+									Fld_num_out = Current_index_in + 1,
+									{Data_for_use_in,New_Index,Fld_num_out,NewMap};
+								(X,_Acc={Data_for_use_in,Index_start_in,Current_index_in,Map_out_list_in}) when X =:= 48->
+									Fld_num_out = Current_index_in + 1,						
+									{Data_for_use_in,Index_start_in,Fld_num_out,Map_out_list_in}
+							 end,
+					   {Rest,Start_index,1,Map_Data_Element},Bitmap_transaction),
+		
+		{_,_,_,FlData}=OutData,
+		io:format("~nkeys and values so far are ~p",[FlData]),
+		FlData.
+
+
+marshall_message()->
+		ok.
--- yapp_test_sock/src/yapp_test_sock_serv.erl
+++ yapp_test_sock/src/yapp_test_sock_serv.erl
@@ -1,3 +1,9 @@
+%%%
+%%% @doc yapp_test_sock_serv module.
+%%%<br>this module is responsible for accepting conections from external interfaces and passing it to the asci processor </br>
+%%% @end
+%%% @copyright Nuku Ameyibor <nayibor@startmail.com>
+
 
 -module(yapp_test_sock_serv).
 -behaviour(gen_server).
@@ -10,13 +16,13 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          code_change/3, terminate/2]).
  
--include_lib("yapp_test_sock/include/yapp_test_sock_spec.hrl").
-
-
+ 
 -define(SOCK(Msg), {tcp, _Port, Msg}).
 -define(TIME, 800).
 -define(EXP, 50).
 -define(BH,2).
+-define(MTI_SIZE,4).
+-define(PRIMARY_BITMAP_SIZE,8).
 
 
 start_link(Socket) ->
@@ -53,104 +59,55 @@ handle_info(?SOCK(Str), S = #state{socket=AcceptSocket,iso_message=Isom}) ->
 		State_new=Isom++Str,
 		%%io:format("~nfull message is ~n~s~nlength is ~p~n",[State_new,length(State_new)]),
 		case length(State_new) of 
-			Size when Size < 2 -> 
+			Size when Size < ?BH -> 
 				%%io:format("smaller size  is ~p,~n~n~n",[State_new]),
 				{noreply, S#state{iso_message=State_new}};
 			_  ->
-				{LenStr, Rest} = lists:split(2, State_new),
-				%%io:format("clength is ~p~n~w~n~s~n",[length(LenStr),LenStr,Rest]),
-				Len = lists:nth(2,LenStr) + 2,
+				{LenStr, Rest} = lists:split(?BH, State_new),
+				io:format("~n --length_lengtstr ~p --legthstr ~w ~n--Rest ~n~s",[length(LenStr),LenStr,Rest]),
+				Len = lists:nth(?BH,LenStr) + ?BH,
 				case length(State_new) of 
-					SizeafterHead when Len =:= SizeafterHead ->
-					Mti_size = 4,
-					Primary_Bitmap_size = 8,
-					%io:format("~n~nlength is ~p ~nfull message is:~n~s", [Len-2,Rest]),
-					%io:format("~n~nrequest_mti:~p",[lists:sublist(Rest,Mti_size)]),
-					%io:format("~n~nfirst twelve integer values are:~p",[lists:sublist(Rest,1,12)]),
-					%io:format("~n~nin progress primary bitmap are:~p",[lists:sublist(Rest,Mti_size+1,8)]),
-					%%gets whether the  first character of the bitmap is 1/0(48,49)cuz of string and then uses this to calculate size of bitmap 
-					Bitmap_size = case lists:nth(1,string:right(integer_to_list(lists:nth(5,Rest),2),8,$0)) of
-									48 -> 8;
-									49 -> 16
-									end,
-					%io:format("~n~nbitmap size is:~p",[Bitmap_size]),
-					%io:format("~n~nbitmap(pri/sec) is :~p",[lists:map(fun(X)->string:right(integer_to_list(X,2),8,$0)end,lists:sublist(Rest,Mti_size+1,Bitmap_size))]),
-					%io:format("~n~nflattend bitmap(pri/sec) in binary is:~p",[lists:flatten(lists:map(fun(X)->string:right(integer_to_list(X,2),8,$0)end,lists:sublist(Rest,Mti_size+1,Bitmap_size)))]),
-					%%io:format("~nfirst value is ~p and first binary bitmap:~p and hex value is ~p",[lists:nth(5,Rest),string:right(integer_to_list(lists:nth(5,Rest),2),8,$0),integer_to_list(lists:nth(5,Rest),16)]),
-					Bitmap_transaction = lists:flatten(lists:map(fun(X)->string:right(integer_to_list(X,2),8,$0)end,lists:sublist(Rest,Mti_size+1,Bitmap_size))),
-					%io:format("~nraw values ~w int value to use  ~p asci value code is:~p",[lists:sublist(Rest,68,8),list_to_integer(lists:sublist(Rest,68,2)),lists:sublist(Rest,68,8)]),
-					
-					%%add bitmap as well as mti to map which holds data elements so they can help in processing rules 
-					Mti_Data_Element = maps:from_list([{ftype,ans},{fld_no,0},{name,<<"Mti">>},{val_binary_form,list_to_binary(lists:sublist(Rest,Mti_size))},{val_list_form,lists:sublist(Rest,Mti_size)}]),
-					Bitmap_Data_ELement = maps:from_list([{ftype,b},{fld_no,0},{name,<<"Bitmap">>},{val_binary_form,list_to_binary(Bitmap_transaction)},{val_list_form,Bitmap_transaction}]),
-					Map_Data_Element1 =  maps:put(<<"_mti">>,Mti_Data_Element,maps:new()), 
-					Map_Data_Element = maps:put(<<"_bitmap">>,Bitmap_Data_ELement,Map_Data_Element1),
-					
-					
-					Start_index = Mti_size+Primary_Bitmap_size+1,
-					%%Bitmap_transaction_test = lists:sublist(Bitmap_transaction,5),
-					OutData =lists:foldl(fun(X,_Acc={Data_for_use_in,Index_start_in,Current_index_in,Map_out_list_in}) when X =:= 49->						
-											    {Ftype,Flength,Fx_var_fixed,Fx_header_length,DataElemName}=?SPEC(Current_index_in),
-												case Fx_var_fixed of
-													fx -> 
-														Data_Element = lists:sublist(Rest,Index_start_in,Flength),
-														New_Index = Index_start_in+Flength ;	
-													vl ->
-														Vl_value = list_to_integer(lists:sublist(Rest,Index_start_in,Fx_header_length)),
-														Start_val = Index_start_in + Fx_header_length , 										
-														Data_Element = lists:sublist(Rest,Start_val,Vl_value),
-														New_Index = Start_val+Vl_value
-												end,
-												NewData  = maps:from_list([{ftype,Ftype},{fld_no,Current_index_in},{name,DataElemName},{val_list_form,Data_Element}]),
-												NewMap = maps:put("_"++integer_to_list(Current_index_in),NewData,Map_out_list_in),
-												Fld_num_out = Current_index_in + 1,
-												{Data_for_use_in,New_Index,Fld_num_out,NewMap};
-											(X,_Acc={Data_for_use_in,Index_start_in,Current_index_in,Map_out_list_in}) when X =:= 48->
-												Fld_num_out = Current_index_in + 1,						
-												{Data_for_use_in,Index_start_in,Fld_num_out,Map_out_list_in}
-										 end,
-								   {Rest,Start_index,1,Map_Data_Element},Bitmap_transaction),
-					
-					{_,_,_,FlData}=OutData,
-					io:format("~nkeys and values so far are ~p",[FlData]),
-					
-					%%reponse message will have to be created here and then sent back based on the request type 
-					%%will start will just changing the mti and then echoing the response back . 
-					
-					
-					%%echo messager for now. no need for all of this .i can just echo back the message received 
-					%%lnestr mti bitmap message 
-					%LengthString will be sent back unc
-					LenStr_Message_Back = LenStr,
-					%%Mti
-					Mti = lists:sublist(Rest,Mti_size),
-					Mti_Message_Back = 	case  Mti of
-											"1200" ->
-												"1210" ;
-											"1220" ->
-												"1230";
-											"1420" ->
-												"1430";
-											_ ->
-												Mti
-										end,			
-					%%Bitmap will be sent back unchanged
-					Bitmap_Message_Back = lists:sublist(Rest,Mti_size+1,Bitmap_size),							
-					%%Data Elements
-					Data_Message_Back = lists:sublist(Rest,Start_index,length(Rest)-(length(Mti_Message_Back)+length(Bitmap_Message_Back))),
-					Send_Message_Back = LenStr_Message_Back ++ Mti_Message_Back ++ Bitmap_Message_Back ++ Data_Message_Back,
-					
-					%%the below statements have to be placed in some sort of block so even if an error occurs shit does not hit the fin
-					send(AcceptSocket,Send_Message_Back),						
-					Message_send_list = yapp_test_lib_dirtyproc:process_message(FlData),
-					
-					%%io:format("~n~nSending List is ~p",[Message_send_list]),
-					%%have to write a function that will filter out the information that will be sent to the client 
-					Status_Send = [{I, (catch gproc:send({n, l, I},{transaction_message,FlData}))} || I <- Message_send_list],
-					%%io:format("~n~nSending Statuses ~p",[Status_Send]),
-					
-						% Display the MTI from the response.
-						{noreply, S#state{iso_message=[]}};
+					SizeafterHead when Len =:= SizeafterHead ->	
+						try yapp_test_ascii_marsh:process_iso_message(Rest) of 
+							FlData ->
+								
+								%%LenStr_Message_Back = LenStr,
+								%%Mti
+								%%Mti = lists:sublist(Rest,?MTI_SIZE),
+								%%Mti_Message_Back = 	case  Mti of
+								%%						"1200" ->
+								%%							"1210" ;
+								%%						"1220" ->
+								%%							"1230";
+								%%						"1420" ->
+								%%							"1430";
+								%%						_ ->
+								%%							Mti
+								%%					end,			
+								%%Bitmap will be sent back unchanged
+								%%Bitmap_Message_Back = lists:sublist(Rest,?MTI_SIZE+1,?PRIMARY_BITMAP_SIZE),	
+								%%Start_index = ?MTI_SIZE+?PRIMARY_BITMAP_SIZE+1,
+								%%Data Elements
+								%%Data_Message_Back = lists:sublist(Rest,Start_index,length(Rest)-(length(Mti_Message_Back)+length(Bitmap_Message_Back))),
+								%%Send_Message_Back = LenStr_Message_Back ++ Mti_Message_Back ++ Bitmap_Message_Back ++ Data_Message_Back,
+								%%the below statements have to be placed in some sort of block so even if an error occurs shit does not hit the fin
+								%%send(AcceptSocket,Send_Message_Back),
+								send(AcceptSocket,State_new),						
+								Message_send_list = yapp_test_lib_dirtyproc:process_message(FlData),
+								%%io:format("~n~nSending List is ~p",[Message_send_list]),
+								%%have to write a function that will filter out the information that will be sent to the client 
+								_Status_Send = [{I, (catch gproc:send({n, l, I},{transaction_message,FlData}))} || I <- Message_send_list],
+								
+								%%io:format("~n~nSending Statuses ~p",[Status_Send]),
+								% Display the MTI from the response.
+						
+								{noreply, S#state{iso_message=[]}}
+						catch
+							error:X ->
+								send(AcceptSocket,State_new),
+								io:format("~nError Message ~p ~nwith input ~p",[erlang:get_stacktrace(),X]),
+								{noreply, S#state{iso_message=[]}}
+						end;
 					SizeafterHead when Len < SizeafterHead ->
 						{noreply, S#state{iso_message=State_new}}
 				end
@@ -167,7 +124,7 @@ handle_info({tcp_error, _Socket, _}, S) ->
     
  %%info coming in from as a result of messages received maybe from othe processes 
 handle_info(E, S) ->
-		io:format("unexpected: ~p~n", [E]),
+		io:format("unexpected message: ~p~n", [E]),
 		{noreply, S}.
 
 code_change(_OldVsn, State, _Extra) ->
