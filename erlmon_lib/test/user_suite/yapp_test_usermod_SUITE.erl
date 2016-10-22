b4c9888f6e9f879f704b15253b26f8308f171e03
Nuku Ameyibor <nayibor@gmail.com>
2016-10-22 14:50:14 +0000

    %%changes 		basic test case added . 		README changed 		process name registered with application

--- erlmon_lib/test/user_suite/yapp_test_usermod_SUITE.erl
+++ erlmon_lib/test/user_suite/yapp_test_usermod_SUITE.erl
@@ -16,11 +16,18 @@ init_per_suite(Config) ->
     application:set_env(mnesia, dir, Priv),
     yapp_test_lib_usermod:install([node()]),
     application:start(mnesia),
-    application:start(yapp_test_lib),
+    application:start(crypto),
+    application:start(bcrypt),
+    application:start(erlpass),
+    application:start(erlmon_lib),
     Config.
 
 end_per_suite(_Config) ->
-    application:stop(mnesia),
+    application:stop(erlmon_lib),
+    application:stop(erlpass),
+    application:stop(bcrypt),
+	application:start(crypto),
+	application:stop(mnesia),
     ok.
 
 init_per_testcase(_, Config) ->
@@ -33,40 +40,40 @@ end_per_testcase(_, _Config) ->
 
 
 add_user(_Config) ->
-	ok = yapp_test_lib_usermod:add_user("nayiborbc@gmail.com",1,123,"Nuku","Ameyibor",1,1).
+	ok = yapp_test_lib_usermod:add_inst(<<"testinstshortname">>,<<"testinstlongname">>,<<"testinst_ident">>), 
+	ok = yapp_test_lib_usermod:add_site(<<"test">>,<<"test">>,1),
+	ok = yapp_test_lib_usermod:add_user("nayiborbc@gmail.com","nuku","ameyibor",1).
 	%%ok = yapp_test_lib_usermod:add_user("nayiborbc@gmail.com",1,123,"Nuku","Ameyibor",1,1).
 
 add_role(_Config) ->
-	ok = yapp_test_lib_usermod:add_role(1,"TRA","Staff").
+	ok = yapp_test_lib_usermod:add_role(<<"TRA">>,<<"Staff">>).
 	
 add_link(_Config) ->
-	ok = yapp_test_lib_usermod:add_link(33,"Customer","get_sales_info_list",true,"Print","Print Stuff").
+	ok = yapp_test_lib_usermod:add_cat(<<"test_cat">>),
+	ok = yapp_test_lib_usermod:add_link("Customer","get_sales_info_list",true,1,"Print",ajax).
 	
 add_role_link(_Config) ->
-	ok = yapp_test_lib_usermod:add_role(2,"ADM","Administrator"),
-	ok = yapp_test_lib_usermod:add_link(33,"Customer","get_sales_info_list",true,"Print","Print Stuff"),
-	ok = yapp_test_lib_usermod:add_role_link(2,33).
+	ok = yapp_test_lib_usermod:add_role(<<"ADM">>,<<"Administrator">>),
+	ok = yapp_test_lib_usermod:add_link("Customer","link_role",true,1,"Print",sidebar),
+	ok = yapp_test_lib_usermod:add_role_link(1,1).
 	
 add_user_role(_Config) ->
-	ok = yapp_test_lib_usermod:add_user("test@gmail.com",1,123,"Nuku","Ameyibor",1,1),
-	ok = yapp_test_lib_usermod:add_role(100,"TRA","Staff"),
-	ok   = yapp_test_lib_usermod:add_user_roles(1,100),
-	{error,check_user_role} = yapp_test_lib_usermod:add_user_roles(121,201).
+	ok = yapp_test_lib_usermod:add_user("nayiborbc222@gmail.com","Nuku","ameyibor",1),
+	ok = yapp_test_lib_usermod:add_role(<<"TRA">>,<<"Staff">>),
+	ok   = yapp_test_lib_usermod:add_user_roles(1,1),
+	{error,check_user_role} = yapp_test_lib_usermod:add_user_roles(121,1500).
 	
 	
 verify_user(_Config) -> 
-  	ok = yapp_test_lib_usermod:add_user("nayiborcheck@gmail.com",1,123,"Nuku","Ameyibor",1,1),
-  	%%ok = yapp_test_lib_usermod:verify_user("nayiborcheck@gmail.com",123),
-  	?assertEqual(ok,yapp_test_lib_usermod:verify_user("nayiborcheck@gmail.com",123)).
+	{ok,Npass} = yapp_test_lib_usermod:reset_pass(1),
+  	{ok,_Id,_Fname,_Site_id,_Inst_id,_Reset_status}=yapp_test_lib_usermod:verify_user("nayiborbc@gmail.com",Npass).
   	
 lock_account(_Config) ->
-	
-  	ok = yapp_test_lib_usermod:add_user("nayiborlock@gmail.com",1,123,"Nuku","Ameyibor",1,1),
-  	{error,check_username_password}=yapp_test_lib_usermod:verify_user("nayiborlock@gmail.com",2),
-  	{error,check_username_password}=yapp_test_lib_usermod:verify_user("nayiborlock@gmail.com",3),
-  	{error,check_username_password}=yapp_test_lib_usermod:verify_user("nayiborlock@gmail.com",4),
-  	{error,check_username_password}=yapp_test_lib_usermod:verify_user("nayiborlock@gmail.com",5),  	
-  	{error,check_username_password_lock}=yapp_test_lib_usermod:verify_user("nayiborlock@gmail.com",5).
-  	
+	{ok,_Npass} = yapp_test_lib_usermod:reset_pass(1),
+  	{error,check_username_password}=yapp_test_lib_usermod:verify_user("nayiborbc@gmail.com","2"),
+  	{error,check_username_password}=yapp_test_lib_usermod:verify_user("nayiborbc@gmail.com","3"),
+  	{error,check_username_password}=yapp_test_lib_usermod:verify_user("nayiborbc@gmail.com","4"),
+  	{error,check_username_password}=yapp_test_lib_usermod:verify_user("nayiborbc@gmail.com","5"),
+  	{error,check_username_password_lock}=yapp_test_lib_usermod:verify_user("nayiborbc@gmail.com","5").
   	
 	
--- erlmon_sock/ebin/erlmon_sock.app
+++ erlmon_sock/ebin/erlmon_sock.app
@@ -2,7 +2,7 @@
  [{description,"Transaction Socket Server"},
   {vsn,"0.1.0"},
   {modules,[yapp_test_sock_app,yapp_test_sock_supersup,yapp_test_ppool_serv,yapp_test_sock_sup,yapp_test_sock_serv,yapp_test_ascii_marsh,yapp_test_ascii_marsh_jpos,yapp_test_sock_test_gen]},
-  {registered, []},
+  {registered, [counter]},
   {mod,{yapp_test_sock_app,[]}},
   {env,[{port, 8005},{name,counter},{limit,10}]},
   {applications,[kernel,stdlib,mnesia,erlmon_lib]}]}.
